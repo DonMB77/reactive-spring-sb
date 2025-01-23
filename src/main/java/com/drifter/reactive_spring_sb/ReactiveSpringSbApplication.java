7e73dab41d251c81f92c7613e90d9168f3a6e139
DonMB77 <purposefuldrifter@gmail.com>
2025-01-23 11:38:58 +0100

    initial init

---
+++ src/main/java/com/drifter/reactive_spring_sb/ReactiveSpringSbApplication.java
@@ -0,0 +1,13 @@
+package com.drifter.reactive_spring_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drifter/reactive_spring_sb/ReactiveSpringSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drifter.reactive_spring_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

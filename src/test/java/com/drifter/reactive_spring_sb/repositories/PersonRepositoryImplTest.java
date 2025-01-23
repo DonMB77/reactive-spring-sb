af0324bd4e256bfd0c3637ecc9dba7aa846227d9
DonMB77 <purposefuldrifter@gmail.com>
2025-01-23 17:42:36 +0100

    PersonRepositoryImplTest mono + flux sandboxing

--- src/test/java/com/drifter/reactive_spring_sb/repositories/PersonRepositoryImplTest.java
+++ src/test/java/com/drifter/reactive_spring_sb/repositories/PersonRepositoryImplTest.java
@@ -3,7 +3,10 @@ package com.drifter.reactive_spring_sb.repositories;
 import com.drifter.reactive_spring_sb.domain.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,5 +36,41 @@ class PersonRepositoryImplTest {
         Mono<Person> personMono = personRepository.getById(1);
 
         personMono.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list -> {
+            list.forEach(person -> System.out.println(person.getFirstName()));
+        });
     }
 }
